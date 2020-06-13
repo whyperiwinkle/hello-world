b69f8fb844e9deb699863cd0216dfcab584db77b
Ryan Scheffer <66504766+whyperiwinkle@users.noreply.github.com>
2020-06-12 18:15:45 -0600

    Another code edit
    Testing web interface.

--- hello-world.sh
+++ hello-world.sh
@@ -4,11 +4,18 @@
 
 # exit
 
-num=0
-while [[ $num -le 6 ]]
-do
-	echo "Hello World! - $num"
-	let "num=num+1"
-done
+# num=0
+# while [[ $num -le 6 ]]
+# do
+# 	echo "Hello World! - $num"
+# 	let "num=num+1"
+# done
 
+# exit
+
+text=( 'Hello' 'World!')
+for x in ${text[@]}
+do
+	echo "$x"
+done
 exit
